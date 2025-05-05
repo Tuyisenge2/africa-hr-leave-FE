b5c0b3f01514f89fe2915ab41047e1bff83ab053
Tuyisenge2 <tuyisengetito3@gmail.com>
2025-05-05 16:27:37 +0200

    ft- dockerizing the app so that it can wokr independntly

--- src/services/authService.ts
+++ src/services/authService.ts
@@ -4,7 +4,7 @@ import { GoogleUser } from "@/validations/google";
 export const authService = {
   async googleSignIn() {
     try {
-      window.location.href = `${process.env.NEXT_PUBLIC_API_URL}/auth/google`;
+      window.location.href = `${process.env.NEXT_PUBLIC_API_URL_3}/auth/google`;
     } catch (error) {
       throw new Error("Failed to authenticate with Google");
     }
